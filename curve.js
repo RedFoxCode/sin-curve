62cd8b8f588a07ef0dcb1a8f70d42bdd549054b7
Gleb Makagonov <bywildeme@gmail.com>
2016-08-17 17:19:13 +0500

    Optimization and other
    Removed random rainbow
    Added interactive (zooming and moving around the graphic)
    Render optimization

--- curve.js
+++ curve.js
@@ -1,7 +1,7 @@
 function initCanvas() {
 	var container = document.getElementsByClassName("container")[0];
 
-	var canvas = document.createElement("canvas");
+	canvas = document.createElement("canvas");
 
 	canvas.width = w = innerWidth;
 	canvas.height = h = innerHeight;
@@ -26,8 +26,8 @@ function getQueue(start, y, step, height, limit, fn) {
 	return queue;
 }
 
-function getColorQueue(limit) { 
-	var frequency = Math.random();
+function getColorQueue(limit) {
+	var frequency = .3;
 
 	var queue = [];
 
@@ -58,6 +58,9 @@ function renderCurve(queue, colorQueue, ctx, step) {
 	while ((y = queue.shift()) != undefined) {
 		x += step;
 
+		if ((y > h) || (y < 0))
+			continue;
+
 		ctx.beginPath();
 		ctx.moveTo(x, y);
 		ctx.lineTo(
@@ -74,15 +77,18 @@ function renderCurve(queue, colorQueue, ctx, step) {
 			7,
 			7
 		);
+
+		if (x > w) 
+			break;
 	}
 }
 
-function curve(height, width, step, limit, ctx, fn) {
+function curve(height, width, step, limit, ctx, fn, start) {
 	ctx.fillStyle = "#222";
 
 	ctx.fillRect(0, 0, w, h);
 
-	var q = getQueue(0, h / 2, step, height, limit, fn);
+	var q = getQueue(start, h / 2, step, height, limit, fn);
 	var c = getColorQueue(limit);
 
 	renderCurve(q, c, ctx, width);
@@ -91,17 +97,41 @@ function curve(height, width, step, limit, ctx, fn) {
 initCanvas();
 
 function newCurve() {
+	if (this != window) {
+		kWidth = 1;
+		kX = 0;
+	}
+
 	var args = [];
 
 	args.push(Number(document.getElementById("input_height").value));
-	args.push(Number(document.getElementById("input_width").value));
+	args.push(Number(document.getElementById("input_width").value) * kWidth);
 	args.push(Number(document.getElementById("input_step").value));
 	args.push(Number(document.getElementById("input_limit").value));
 
 	args.push(ctx);
 	args.push(Math[document.getElementById("input_fn").value.toLowerCase()]);
 
+	args.push(kX);
+
 	curve.apply(this, args);
 }
+
+var kWidth = 1;
+
+var kX = 0;
+
+canvas.addEventListener("mousewheel", function(e) {
+	kWidth += -e.deltaY / 1000;
+
+	newCurve();
+});
+
+canvas.addEventListener("mousedown", function(e){
+    kX += -(e.clientX > w / 2) || 1;
+
+    newCurve();
+});
+
 
 newCurve();
